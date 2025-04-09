747090e667371640c3e1946fcff25555f9666d7b
femaledurk <chelseataylor.moore25@compscihigh.org>
2025-04-09 19:26:26 +0000

    blhblh

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/add/teacher", async (req, res) => {
 
 // Add your SRV string, make sure that the database is called CSHteachers
 async function startServer() {
-  await mongoose.connect("...");
+  await mongoose.connect("mongodb+srv://SE12:CSH2025@chelzz.z7zzp.mongodb.net/?retryWrites=true&w=majority&appName=chelzz"
 
   app.listen(3000, () => {
     console.log(`Server running.`);
